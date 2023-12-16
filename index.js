2ec430b83c95238003b44a000a3d7ab2c93271a0
Dronevich <gob1fprog@gmail.com>
2023-12-16 13:00:47 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    elem = document.getElementById('root');
+    elem.classList.add("mystyle");
+    elem.innerHTML = "Help ....."; 
+    console.log(document);
+    console.log(window);
+};
